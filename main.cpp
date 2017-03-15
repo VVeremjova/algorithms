5ca9ceac32b40fac8d53c81106c7f02dcedea52e
Veronica Veremjova (GitHub) <vveremjova@luxoft.com>
2017-03-15 21:46:20 +0200

    fix binary search

--- main.cpp
+++ main.cpp
@@ -23,132 +23,148 @@ int naive_search_1(std::vector<int>& v, int key) {
    if (i!=v.size()) {
        return i;
    }
-
    return -1;
 }
 
-//int binary_search(const std::vector<int>& A, int key, int first = -1, int last = -1) {
+int binary_search_0(const std::vector<int>& A, int key, int first = -1, int last = -1) {
 
-//   if (first==-1) {
-//       first = 0;
-//   }
-//   if (last = -1) {
-//       last = size;
-//   }
+   if (first==-1) {
+       first = 0;
+   }
+   if (last == -1) {
+       last = A.size();
+   }
+   int i = (last + first)/2;
 
-//   int i = (last - first)/2 +1;
-//      cout<<"i ="<<i<<endl;
-//   for (; i< A.size; ++i) {
-//       if (A[i]==key) {
-////           cout<<"Stop here \n";
-//           return i;
-//       }
-//       if (first == last)
-//       {
-//           cout<<"Element wasn't found"<<endl;
-//           return -1;
-//       }
-//       if(key < A[i]) {
+   for (; i< A.size(); ++i) {
+       if (A[i]==key) {
+           return i;
+       }
+       if (first == last)
+       {
+           cout<<"Element wasn't found"<<endl;
+           return -1;
+       }
+       if(key < A[i]) {
+         last = i-1;
+//       cout<<"left "<<first<<",element: "<<A[i]<<", right = "<<last<<endl;
 
-//           last = i;
-//           cout<<"left "<<first<<",element: "<<A[i]<<", right = "<<last<<endl;
-
-//           return binary_search(A, i, key, first, last);
-//       }
-//       if(key >A[i]) {
-
-//         first = i+1;
+           return binary_search_0(A,  key, first, last);
+       }
+       if(key >A[i]) {
+         first = i+1;
 //          cout<<"left "<<first<<",element: "<<A[i]<<", right = "<<last<<endl;
-//         return  binary_search(A, size, key, first, last);
-//       }
-//   }
-//   return -1;
-//}
-
-//void test_suit1() {
-
-//    int A[]={2,4,6,7,8,11,24,56,78,80};
-//    //cout<<naive_search(A,10,24)<<'\n';
-//    cout<<binary_search(A,10,24)<<'\n';
-//}
-
-//void test_suit4() {
-
-//    int A[]={2,4,6};
-//    //cout<<naive_search(A,10,24)<<'\n';
-//    cout<<binary_search(A,3,2)<<'\n';
-//}
-
-//void test(expect, f, params) {
-//    auto got(f(params));
-//    if (got ~= expect) {
-//        cerr<< "Failed: expected" << expect<<" != actual" << got <<endl;
-//    }
-//}
-
-template <class T>
-ostream & operator << (ostream&o ,const  vector<T>&v) {
-    o<<"[";
-    for (auto& x:v) {
-        o<< x<< ", " ;
-    }
+         return  binary_search_0(A, key, first, last);
+       }
+   }
+   return -1;
 }
 
-template <class TFunc, class TResult, class TParam1>
-void test(TResult expect, TFunc f, TParam1 params) {
-    auto got= f(params);
+
+template <class TFunc, class TResult, class TParam1, class TParam2>
+void test(TResult expect, TFunc f, TParam1 param1,  TParam2 param2) {
+    auto got= f(param1, param2);
     if (got != expect) {
-        cerr<< "Failed: f(" << params<< "," << expect<<" != actual" << got <<endl;
+        cerr<< "Failed:" <<" expected: " << expect<<", actual: " << got <<endl;
+    }
+    else {
+        cout<<"Test passed!" <<endl;
     }
 }
 
-//template <class TFunc, class TResult, class TParam1, class TParam2>
-//void test(TResult expect, TFunc f, TParam1 params, TParam2 p2) {
-//    auto got= f(params);
-//    if (got ~= expect) {
-//        cerr<< "Failed: expected" << expect<<" != actual" << got <<endl;
-//    }
-//}
 
-void test_search() {
-    typedef vector<int> Arr;
-    test (8, naive_search_1, Arr({0,1,2,3,4,5,7,8}));
-}
-
-void test_suit5() {
-
-//    int A[]={2,4,6};
-    //cout<<naive_search(A,10,24)<<'\n';
-//    cout<<binary_search(A,3,4)<<'\n';
-    std::vector<int> A;
-    A.push_back(2);
-    A.push_back(4);
-    A.push_back(6);
-    cout<<naive_search_0(A,4)<<'\n';
+template <class TFunc, class TResult, class TParam1, class TParam2, class TParam3>
+void test(TResult expect, TFunc f, TParam1 param1,  TParam2 param2, TParam3 param3, TParam3 param4) {
+    auto got= f(param1, param2, param3, param4);
+    if (got != expect) {
+        cerr<< "Failed:" <<" expected: " << expect<<", actual: " << got <<endl;
+    }
+    else {
+        cout<<"Test passed!" <<endl;
+    }
 }
 
 
+template <int N>
+void test_suit();
 
-//void test_suit6() {
+template <>
+void test_suit<0>() {
 
-//    int A[]={2,4,6};
-//    //cout<<naive_search(A,10,24)<<'\n';
-//    cout<<binary_search(A,3,6)<<'\n';
-//}
+    std::vector<int> A={2,4,6,7,8,11,24,56,78,80};
+    test(3, naive_search_0, A, 7);
+    test(3, binary_search_0, A, 7, -1, -1);
+}
+
+template <>
+void test_suit<1>() {
+    std::vector<int> A={2,4,6,7,8,11,24,56,78,80};
+    test(6, naive_search_0, A, 24);
+    test(6, binary_search_0, A, 24, -1, -1);
+}
+
+
+template <>
+void test_suit<2>() {
+    std::vector<int> A ={2,4,6};
+    test(0, naive_search_0, A, 2);
+    test(0, binary_search_0, A, 2, -1, -1);
+}
+
+template <>
+void test_suit<3>() {
+    std::vector<int> A = {2, 4, 6};
+    test(1, naive_search_0, A, 4);
+    test(1, binary_search_0, A, 4, -1, -1);
+}
+
+
+
+template <>
+void test_suit<4>() {
+    std::vector<int> A ={2,4,6};
+    test(2, naive_search_0, A, 6);
+    test(2, binary_search_0, A, 6, -1, -1);
+}
+
+
+template <>
+void test_suit<5>() {
+    std::vector<int> A ={2,4};
+    test(1, naive_search_0, A, 4);
+    test(1, binary_search_0, A, 4, -1, -1);
+}
+
+
+
+template <>
+void test_suit<6>() {
+    std::vector<int> A ={2,4};
+    test(-1, naive_search_0, A, 3);
+    test(-1, binary_search_0, A, 3, -1, -1);
+}
+
+
+template <int N>
+void check_tests();
+
+template <>
+void check_tests<0>()
+{
+    cout<<"Test #0"<<endl;
+    test_suit<0>();
+}
+
+template <int N>
+void check_tests()
+{
+  cout<<"Test #"<<N<<endl;
+  test_suit<N>();
+  check_tests<N - 1>();
+};
+
 int main(int argc, char *argv[])
 {
-//    cout << "1st test" << endl;
-//     test_suit1();
-//     cout <<"2nd test:" << endl;
-//    test_suit2();
-//    cout <<"3rd test:" << endl;
-//     test_suit3();
-
-    cout << "1st test" << endl;
-//     test_suit4();
-     cout <<"2nd test:" << endl;
-    test_suit5();
-    cout <<"3rd test:" << endl;
-//     test_suit6();
+    check_tests<6>();
     return 0;
 }
