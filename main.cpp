6a48f062248a1338090e9d655caf45586cc9b8b6
Veronica <vveremjova@luxoft.com>
2017-03-16 16:52:49 +0200

    Tests for naive seacrh

--- main.cpp
+++ main.cpp
@@ -88,16 +88,46 @@ void test(TResult expect, TFunc f, TParam1 param1,  TParam2 param2, TParam3 para
 template <int N>
 void test_suit();
 
-template <>
-void test_suit<0>() {
 
-    std::vector<int> A={2,4,6,7,8,11,24,56,78,80};
-    test(3, naive_search_0, A, 7);
-    test(3, binary_search_0, A, 7, -1, -1);
-}
+//template <>
+//void test_suit<0>() {
+
+//    // Not exists
+//    typedef vector<int> Array;
+//    auto key = 8;
+//    //degenerate
+//    test(-1, naive_search_0, Array(), key);
+//    test(-1, naive_search_0, Array({key-1}), key);
+//    test(-1, naive_search_0, Array({key-1, key+1}), key);
+//    test(-1, naive_search_0, Array({1, 2, 3, 4, ,5, 7}), key);
+//    test(-1, naive_search_0, Array({9, 10, 11, 12}), key);
+//    test(-1, naive_search_0, Array({4, 1, 2, 7, 10}), key);
+
+
+//    //key exists
+//    test(0, naive_search_0, Array({key}), key);
+//    test(0, naive_search_0, Array({key, key+1}), key);
+//    test(1, naive_search_0, Array({key-1, key}), key);
+//    test(7, naive_search_0, Array({0, 1, 2, 3, 4, 5, 7, key}), key);
+//    test(0, naive_search_0, Array({key, 9, 10, 11, 12}), key);
+//    test(2, naive_search_0, Array({4, 1, key, 2, 7, 10}), key);
+
+//    test(0, naive_search_0, Array({key, 1, key, 10, 11, 12}), key);
+//    test(2, naive_search_0, Array({4, 1, key, 2,key, 7, 10}), key);
+
+//}
+//template <>
+//void test_suit<0>() {
+
+//    //degenerate
+//    std::vector<int> A={2,4,6,7,8,11,24,56,78,80};
+//    test(3, naive_search_0, A, 7);
+//    test(3, binary_search_0, A, 7, -1, -1);
+//}
 
 template <>
 void test_suit<1>() {
+    //trivial
     std::vector<int> A={2,4,6,7,8,11,24,56,78,80};
     test(6, naive_search_0, A, 24);
     test(6, binary_search_0, A, 24, -1, -1);
@@ -106,6 +136,7 @@ void test_suit<1>() {
 
 template <>
 void test_suit<2>() {
+    //trivial-2
     std::vector<int> A ={2,4,6};
     test(0, naive_search_0, A, 2);
     test(0, binary_search_0, A, 2, -1, -1);
@@ -113,6 +144,7 @@ void test_suit<2>() {
 
 template <>
 void test_suit<3>() {
+    //general
     std::vector<int> A = {2, 4, 6};
     test(1, naive_search_0, A, 4);
     test(1, binary_search_0, A, 4, -1, -1);
@@ -145,26 +177,63 @@ void test_suit<6>() {
 }
 
 
-template <int N>
-void check_tests();
+//template <int N>
+//void check_tests();
 
-template <>
-void check_tests<0>()
-{
-    cout<<"Test #0"<<endl;
-    test_suit<0>();
-}
+//template <>
+//void check_tests<0>()
+//{
+//    cout<<"Test #0"<<endl;
+//    test_suit<0>();
+//}
 
-template <int N>
-void check_tests()
-{
-  cout<<"Test #"<<N<<endl;
-  test_suit<N>();
-  check_tests<N - 1>();
-};
+//template <int N>
+//void check_tests()
+//{
+//  cout<<"Test #"<<N<<endl;
+//  test_suit<N>();
+//  check_tests<N - 1>();
+//};
 
 int main(int argc, char *argv[])
 {
-    check_tests<6>();
+//    check_tests<6>();
+
+
+
+    // test cases:
+    /*key exists
+     * degenerate - not exists
+     * trivial
+     * trivial-2
+     * general
+     *
+     * key not exists
+     * degenerate
+     * trivial
+     * trivial-2
+     * general
+     */
+
+    typedef std::vector<int> Array;
+       auto key = 8;
+    test(-1, naive_search_0, Array(), key);
+    test(-1, naive_search_0, Array({key-1}), key);
+    test(-1, naive_search_0, Array({key-1, key+1}), key);
+    test(-1, naive_search_0, Array({1, 2, 3, 4, 5, 7}), key);
+    test(-1, naive_search_0, Array({9, 10, 11, 12}), key);
+    test(-1, naive_search_0, Array({4, 1, 2, 7, 10}), key);
+
+
+    //key exists
+    test(0, naive_search_0, Array({key}), key);
+    test(0, naive_search_0, Array({key, key+1}), key);
+    test(1, naive_search_0, Array({key-1, key}), key);
+    test(7, naive_search_0, Array({0, 1, 2, 3, 4, 5, 7, key}), key);
+    test(0, naive_search_0, Array({key, 9, 10, 11, 12}), key);
+    test(2, naive_search_0, Array({4, 1, key, 2, 7, 10}), key);
+
+    test(0, naive_search_0, Array({key, 1, key, 10, 11, 12}), key);
+    test(2, naive_search_0, Array({4, 1, key, 2,key, 7, 10}), key);
     return 0;
 }
