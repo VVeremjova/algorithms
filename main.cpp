a8a31d97768953792a103af68e61e87ba81dfda2
Veronica Veremjova (GitHub) <vveremjova@luxoft.com>
2017-03-26 23:07:49 +0300

    Lower and upper bound algorithms

--- main.cpp
+++ main.cpp
@@ -3,232 +3,99 @@
 #include <algorithm>
 #include <assert.h>
 
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
 using namespace std;
+template <class Iter, class ParamType>
 
-int naive_search_0(const std::vector<int>& v, int key) {
-
-   for (int i = 0; i<v.size(); ++i) {
-       if (v[i] == key) {
-           return i;
-       }
-   }
-   return -1;
+Iter lower_bound_1(Iter first, Iter last, const ParamType &key) {
+    while (first<last) {
+        Iter m = first + (last-first)/2;
+        if ( *m <key ) {
+            first = m+1;
+        }
+        else {
+            last = m;
+        }
+    }
+    return first;
 }
 
-int naive_search_1(std::vector<int>& v, int key) {
-   v.push_back(key);
-   int i =0;
-   while (v[i] != key) {
-        ++i;
-   }
-   v.pop_back();
-   if (i!=v.size()) {
-       return i;
-   }
-   return -1;
+template <class Iter, class ParamType>
+
+Iter upper_bound_1(Iter first, Iter last, const ParamType &key) {
+    // [a,b) = [a,m)U[m]U(m,b)
+    while (first<last) {
+        Iter m = first + (last-first)/2;
+        if ( key >= *m ) { //  or (!(key<m))
+            //[a, m+1)U[m+1,key,last)
+            first = m+1;
+        }
+        else {
+            last = m;
+        }
+    }
+    return last;
 }
 
-int binary_search_0(const std::vector<int>& A, int key, int first = -1, int last = -1) {
+template <class Iter, class ParamType>
 
-   if (first==-1) {
-       first = 0;
-   }
-   if (last == -1) {
-       last = A.size();
-   }
-   int i = (last + first)/2;
-
-   for (; i< A.size(); ++i) {
-       if (A[i]==key) {
-           return i;
-       }
-       if (first == last)
-       {
-           cout<<"Element wasn't found"<<endl;
-           return -1;
-       }
-       if(key < A[i]) {
-         last = i-1;
-        // cout<<"left "<<first<<",element: "<<A[i]<<", right = "<<last<<endl;
-
-           return binary_search_0(A,  key, first, last);
-       }
-       if(key >A[i]) {
-         first = i+1;
-        // cout<<"left "<<first<<",element: "<<A[i]<<", right = "<<last<<endl;
-         return  binary_search_0(A, key, first, last);
-       }
-   }
-   return -1;
+Iter binary_search_3(Iter first, Iter last, const ParamType &key) {
+    Iter v = lower_bound_1(first, last, key);
+    return (v!=last) && !(key < *v ) ? v : last;
 }
 
-int binary_search_1(const std::vector<int>& v, size_t begin, size_t end, int key) {
-    assert(std::is_sorted(v.begin(), v.end()));
+template <class Iter, class ParamType>
 
-    if (begin == end) return -1;
-    if ((end - begin) == 1) {
-        if (v[begin] == key)
-            return begin;
-        else
-            return -1;
-    }
-
-    size_t b = 0;
-    size_t m = (begin+end)/2;
-    // [b, e) = [b,m) U [m,e)
-
-    if (key <v [m]) {
-     return  binary_search_1(v, begin, m, key);
-    }
-    else if (v[m] <key) {
-         return  binary_search_1(v, m, end, key);
-    }
-     else {
-        return m;
-    }
-
+Iter binary_search_4(Iter first, Iter last, const ParamType &key) {
+    Iter v = upper_bound_1(first, last, key);
+    return (v!=last) && !(key >= *v ) ? v : last-1;
 }
 
-template <class TFunc, class TResult, class TParam1, class TParam2>
+template <class TFunc, class TResult, class TParam1,  class TParam2 >
+
 void test(TResult expect, TFunc f, TParam1 param1,  TParam2 param2) {
+
     auto got= f(param1, param2);
+
     if (got != expect) {
+
         cerr<< "Failed:" <<" expected: " << expect<<", actual: " << got <<endl;
+
     }
+
     else {
+
         cout<<"Test passed!" <<endl;
+
     }
+
 }
 
-
-template <class TFunc, class TResult, class TParam1, class TParam2, class TParam3>
-void test(TResult expect, TFunc f, TParam1 param1,  TParam2 param2, TParam3 param3, TParam3 param4) {
-    auto got= f(param1, param2, param3, param4);
-    if (got != expect) {
-        cerr<< "Failed:" <<" expected: " << expect<<", actual: " << got <<endl;
-    }
-    else {
-        cout<<"Test passed!" <<endl;
-    }
-}
-
-
-template <int N>
-void test_suit();
-
-
-//template <>
-//void test_suit<0>() {
-
-//    // Not exists
-//    typedef vector<int> Array;
-//    auto key = 8;
-//    //degenerate
-//    test(-1, naive_search_0, Array(), key);
-//    test(-1, naive_search_0, Array({key-1}), key);
-//    test(-1, naive_search_0, Array({key-1, key+1}), key);
-//    test(-1, naive_search_0, Array({1, 2, 3, 4, ,5, 7}), key);
-//    test(-1, naive_search_0, Array({9, 10, 11, 12}), key);
-//    test(-1, naive_search_0, Array({4, 1, 2, 7, 10}), key);
-
-
-//    //key exists
-//    test(0, naive_search_0, Array({key}), key);
-//    test(0, naive_search_0, Array({key, key+1}), key);
-//    test(1, naive_search_0, Array({key-1, key}), key);
-//    test(7, naive_search_0, Array({0, 1, 2, 3, 4, 5, 7, key}), key);
-//    test(0, naive_search_0, Array({key, 9, 10, 11, 12}), key);
-//    test(2, naive_search_0, Array({4, 1, key, 2, 7, 10}), key);
-
-//    test(0, naive_search_0, Array({key, 1, key, 10, 11, 12}), key);
-//    test(2, naive_search_0, Array({4, 1, key, 2,key, 7, 10}), key);
-
-//}
-//template <>
-//void test_suit<0>() {
-
-//    //degenerate
-//    std::vector<int> A={2,4,6,7,8,11,24,56,78,80};
-//    test(3, naive_search_0, A, 7);
-//    test(3, binary_search_0, A, 7, -1, -1);
-//}
-
-template <>
-void test_suit<1>() {
-    //trivial
-    std::vector<int> A={2,4,6,7,8,11,24,56,78,80};
-    test(6, naive_search_0, A, 24);
-    test(6, binary_search_0, A, 24, -1, -1);
-}
-
-
-template <>
-void test_suit<2>() {
-    //trivial-2
-    std::vector<int> A ={2,4,6};
-    test(0, naive_search_0, A, 2);
-    test(0, binary_search_0, A, 2, -1, -1);
-}
-
-template <>
-void test_suit<3>() {
-    //general
-    std::vector<int> A = {2, 4, 6};
-    test(1, naive_search_0, A, 4);
-    test(1, binary_search_0, A, 4, -1, -1);
-}
-
-
-
-template <>
-void test_suit<4>() {
-    std::vector<int> A ={2,4,6};
-    test(2, naive_search_0, A, 6);
-    test(2, binary_search_0, A, 6, -1, -1);
-}
-
-
-template <>
-void test_suit<5>() {
-    std::vector<int> A ={2,4};
-    test(1, naive_search_0, A, 4);
-    test(1, binary_search_0, A, 4, -1, -1);
-}
-
-
-
-template <>
-void test_suit<6>() {
-    std::vector<int> A ={2,4};
-    test(-1, naive_search_0, A, 3);
-    test(-1, binary_search_0, A, 3, -1, -1);
-}
-
-
-//template <int N>
-//void check_tests();
-
-//template <>
-//void check_tests<0>()
-//{
-//    cout<<"Test #0"<<endl;
-//    test_suit<0>();
-//}
-
-//template <int N>
-//void check_tests()
-//{
-//  cout<<"Test #"<<N<<endl;
-//  test_suit<N>();
-//  check_tests<N - 1>();
-//};
-
-int main(int argc, char *argv[])
+int main()
 {
-//    check_tests<6>();
+    typedef std::vector<int> Array;
+    auto search = [](const vector<int>&v, int key) {
+        auto r = binary_search_3(v.begin(),v.end(),key);
+        return r!= v.end()? r-v.begin() :-1;
+    };
 
+    auto search2 = [](const vector<int>&v, int key) {
+        auto r = binary_search_4(v.begin(),v.end(),key);
+        return r!= v.end()? r-v.begin() : -1;
+    };
 
+    auto key = 8;
+  //  Array a ({key-1});
+   // Array::iterator i =  upper_bound_1(a.begin(), a.end(), key);
+   // cout<< i-a.begin() <<endl;
 
+    //Array::iterator i2 =  binary_search_3(a.begin(), a.end(), key);
+   //  cout<< i2-a.begin() <<endl;
     // test cases:
     /*key exists
      * degenerate - not exists
@@ -243,36 +110,58 @@ int main(int argc, char *argv[])
      * general
      */
 
-    typedef std::vector<int> Array;
-       auto key = 8;
-    test(-1, naive_search_0, Array(), key);
-    test(-1, naive_search_0, Array({key-1}), key);
-    test(-1, naive_search_0, Array({key-1, key+1}), key);
-    test(-1, naive_search_0, Array({1, 2, 3, 4, 5, 7}), key);
-    test(-1, naive_search_0, Array({9, 10, 11, 12}), key);
-    test(-1, naive_search_0, Array({4, 1, 2, 7, 10}), key);
+   // test(-1, search, Array(), key);
+    //key exists
 
+    test(0, search, Array({key}), key);
+    test(0, search, Array({key, key+1}), key);
+    test(1, search, Array({key-1, key}), key);
+    test(2, search, Array({key-2, key-1,key}), key);
+    test(8, search, Array({0,1,2,3,4,5,6,7,key}), key);
+    test(0, search, Array({key, 9, 10, 11, 12}), key);
+    test(2, search, Array({2,4, key, 11, 12}), key);
+    test(0, search, Array({key, key, key+1, key+2}), key);
+    test(2, search, Array({key-2, key-1, key, key+2}), key);
+    test(2, search, Array({1, 7, key, key}), key);
+
+    // key not exists
+    cout<<"==== key not exists"<<endl;
+    test(-1, search, Array(), key);
+    test(-1, search, Array({key-1}), key);
+    test(-1, search, Array({key+1}), key);
+    test(-1, search, Array({key-2, key-1}), key);
+    test(-1, search, Array({key+1, key+2}), key);
+    test(-1, search, Array({key-1, key+1}), key);
+    test(-1, search, Array({1,2,3,4,5,6,7}), key);
+    test(-1, search, Array({9,10,11,12}), key);
+
+//    // key not exists
+
+    cout<<"==== key not exists - for upperbound search"<<endl;
+    test(-1, search2, Array(), key);
+    test(0, search2, Array({key-1}), key);
+    test(0, search2, Array({key+1}), key);
+    test(1, search2, Array({key-2, key-1}), key);
+    test(0, search2, Array({key+1, key+2}), key);
+    test(1, search2, Array({key-1, key+1}), key);
+    test(6, search2, Array({1,2,3,4,5,6,7}), key);
+    test(0, search2, Array({9,10,11,12}), key);
 
     //key exists
-    test(0, naive_search_0, Array({key}), key);
-    test(0, naive_search_0, Array({key, key+1}), key);
-    test(1, naive_search_0, Array({key-1, key}), key);
-    test(7, naive_search_0, Array({0, 1, 2, 3, 4, 5, 7, key}), key);
-    test(0, naive_search_0, Array({key, 9, 10, 11, 12}), key);
-    test(2, naive_search_0, Array({4, 1, key, 2, 7, 10}), key);
 
-    test(0, naive_search_0, Array({key, 1, key, 10, 11, 12}), key);
-    test(2, naive_search_0, Array({4, 1, key, 2,key, 7, 10}), key);
-
-
-    test(-1, binary_search_1, Array(), 0, 0, key); // const std::vector<int>& v, size_t begin, size_t end, int key);
-    test(-1, binary_search_1, Array({1, 2, 3, 4, 5, 7}), 0, 6, key);
-    test(0, binary_search_1, Array({8, 9, 18}), 0, 3, key);
-    test(1, binary_search_1, Array({1, 8, 10}), 0, 3, key);
-    test(2, binary_search_1, Array({1, 2, 8}), 0, 3, key);
-
-    test(0, binary_search_1, Array({key, 9, 10, 11, 12}), 0, 5, key);
-    test(2, binary_search_1, Array({4, 5, key, 11, 17, 20}), 0, 6, key);
+    cout<<"==== key exists - for upperbound search"<<endl;
+    test(0, search2, Array({key}), key);
+    test(1, search2, Array({key, key+1}), key);
+    test(1, search2, Array({key-1, key}), key);
+    test(2, search2, Array({key-2, key-1,key}), key);
+    test(8, search2, Array({0,1,2,3,4,5,6,7,key}), key);
+    test(1, search2, Array({key, 9, 10, 11, 12}), key);
+    test(3, search2, Array({2,4, key, 11, 12}), key);
+    test(2, search2, Array({key, key, key+1, key+2}), key);
+    test(3, search2, Array({key-2, key-1, key, key+2}), key);
+    test(3, search2, Array({1, 7, key, key}), key);
+    test(4, search2, Array({1, 7, key, key, 10}), key);
 
     return 0;
+
 }
